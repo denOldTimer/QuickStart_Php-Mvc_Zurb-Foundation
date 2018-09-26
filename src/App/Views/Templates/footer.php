982dd876a70a3b48440f36b1204baa93c84bd489
ScorpioCoding <kribo@outlook.com>
2018-09-26 11:58:33 +0200

    transfer

---
+++ src/App/Views/Templates/footer.php
@@ -0,0 +1,7 @@
+
+  <footer>
+    <div><?php echo $scCopyright; ?></div>
+  </footer>
+  <script src="/public/js/app.js"></script>
+</body>
+</html>
---
+++ src/public/js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+import 'foundation-sites/js/foundation'
+
+const $ = require('jquery')
+
+$(document).foundation()
+
+$(document).ready(function () {
+
+
+})
